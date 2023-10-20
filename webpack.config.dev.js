4703b1cffc2cfd891b43eb4bb2b45ef80156d0a2
daniel <daniel@Daniels-MBP.home>
2023-10-20 13:34:47 +0200

    chore: fixing build errors

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, '/src/index.ts'),
