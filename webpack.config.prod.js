a9e7749d8538e2a478a041cf9f1e4ec0e4fbc094
violettomania <danielpolya@protonmail.com>
2023-10-22 18:26:53 +0200

    build: add index.prod.html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'index.prod.html',
       filename: 'index.html',
     }),
     new CopyWebpackPlugin({
